9710f55c52298b6433ef839b617e507166750872
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-03-30 03:46:27 +0700

    fix connection mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,22 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { ProfileUserModule } from './profile-user/profile-user.module';
-import { ProfileUser } from './profile-user/entities/profile-user.entity';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'test',
-    entities: [User, ProfileUser],
-    synchronize: true,
-  }),
+  imports: [
+  //   TypeOrmModule.forRoot({
+  //   type: 'mysql',
+  //   host: 'localhost',
+  //   port: 3306,
+  //   username: 'root',
+  //   password: 'root',
+  //   database: 'test',
+  //   entities: [User, ProfileUser],
+  //   synchronize: true,
+  // }),
   JwtModule.register({
     global : true,
     secret:  process.env.JWT_SECRET,
